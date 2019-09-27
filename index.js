2c449c1269cb9e690784736a83d806f36ae12844
jakep5 <jakepagel1@yahoo.com>
2019-09-27 13:24:40 -0500

    updating branch

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("a.scroll-down").on("click", function(e) {
+      e.preventDefault();
+      $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+      }, 600);
+    });
+  });
+
+
+/* $("a.scroll-down").click(function() {
+    $("div.mainWrapper").animate({
+        scrollTop: $("div.bioWrapper").offset().top
+    }, 2000);
+});
+ */
+/* function initializePage() {
+}
+
+$(initializePage()); */
