f91fc43bb13bd24865f60f0ff7c1bd09d029e99c
jakep5 <jakepagel1@yahoo.com>
2019-09-30 12:09:11 -0500

    further stylized for smaller screens

--- index.js
+++ index.js
@@ -16,13 +16,57 @@ $(document).ready(function() {
   });
 });
 
-/* $("a.scroll-down").click(function() {
-    $("div.mainWrapper").animate({
-        scrollTop: $("div.bioWrapper").offset().top
-    }, 2000);
+$(document).ready(function() {
+  $("a.scroll-downSkills").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+  });
 });
- */
-/* function initializePage() {
+
+$(document).ready(function() {
+  $("a.scroll-downProjects").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+  });
+});
+
+$(document).ready(function() {
+  $("a.scroll-downTop").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+  });
+});
+
+$(document).ready(function() {
+  $("#contactInfo").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({ 
+      scrollTop: $("div.contactInfoWrapper").offset().top 
+    }, 600);
+  });
+});
+
+
+$(document).ready(function() {
+  $("#github").click(function() {
+    window.open("https://github.com/jakep5/")
+  })
+});
+
+$(document).ready(function() {
+  $("#email").click(function() {
+    window.location.href = "mailto:jakepagel1@yahoo.com";
+  })
+});
+
+
+function initializePage() {
 }
 
-$(initializePage()); */
+$(initializePage());
